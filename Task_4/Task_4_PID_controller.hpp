aa8c9f1fba859eb49f120ed4b48e0a1e8ff9180e
Victor Lim <z5482422@gmail.com>
2025-08-15 20:15:34 +1000

    Task4.ino and all header files are ready from Task_3 Lidar and Turning

--- Task_4/Task_4_PID_controller.hpp
+++ Task_4/Task_4_PID_controller.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "motor.hpp"
-#include "DualEncoder.hpp"
-#include "EncoderOdometry.hpp"
+#include "Task_4_motor.hpp"
+#include "Task_4_DualEncoder.hpp"
+#include "Task_4_EncoderOdometry.hpp"
 
 #include <math.h>
 
@@ -17,70 +17,79 @@ public:
     PIDController(float kp, float ki, float kd) : kp(kp), ki(ki), kd(kd) {}
 
     // Compute the output signal required from the current/actual value.
-    
+    // convert from microseconds to seconds
     float compute(float input) {
-      // convert from microseconds to seconds
-      currTime = micros();
-      dt = static_cast<float>(currTime - prevTime) / 1e6;
-      prevTime = currTime;
+        curr_time = micros();
+        dt = static_cast<float>(curr_time - prev_time) / 1e6;
+        prev_time = curr_time;
 
-      // Check division by zero
-      if (dt == 0) {
-          dt = 1e-6; 
-      }
+        // Check division by zero
+        if (dt == 0) {
+            dt = 1e-6; 
+        }
 
-      error = setpoint - input;
+        error = setpoint - (input - zero_ref);
 
-      // Proportional term
-      float p_out = kp * error;
+        // Proportional term
+        float p_out = kp * error;
 
-      // Integral term
-      integral += error * dt;
-      float i_out = ki * integral;
+        // Integral term
+        integral += error * dt;
+        float i_out = ki * integral;
 
-      // Derivative term
-      derivative = (error - prevError) / dt;
-      float d_out = kd * derivative;
+        // Derivative term
+        derivative = (error - prev_error) / dt;
+        float d_out = kd * derivative;
 
-      // COmbine
-      output = p_out + i_out + d_out;
+        // Combine
+        output = p_out + i_out + d_out;
 
-      prevError = error;
+        prev_error = error;
 
-      int signCheck = output < 0 ? -1 : 1;
+        int signCheck = output < 0 ? -1 : 1;
 
-      float finalOutput = output / REDUPWM;
-      if (abs(finalOutput) < MINPWM) {
-        finalOutput = (finalOutput >= 0) ? MINPWM : -MINPWM;
-      }
-
-      return finalOutput;
+        return signCheck * (output / REDUPWM < MINPWM ? MINPWM : output / REDUPWM);
     }
 
     float getError() {
-      return error;
+        return error;
+    }
+
+    void tune(float p, float i, float d) {
+        kp = p;
+        ki = i;
+        kd = d;
+    }
+
+    void zeroTarget(float zero, float target) {
+        // Reset integral, start new movement
+        integral = 0;
+        prev_error = 0;
+        prev_time = micros();
+        zero_ref = zero;
+        setpoint = target;
     }
 
     void newTarget(float target) {
         // Reset integral, start new movement
         integral = 0;
-        prevError = 0;
-        prevTime = micros();
+        prev_error = 0;
+        prev_time = micros();
         //zero_ref = zero;
         setpoint = target;
     }
 
 
 public:
-    uint32_t prevTime, currTime = micros();
+    uint32_t prev_time, curr_time = micros();
     float dt;
 
 private:
     float kp, ki, kd;
     float error, derivative, integral, output;
-    float prevError = 0;
+    float prev_error = 0;
     float setpoint = 0;
-    //float zero_ref = 0;
+    float zero_ref = 0;
 };
 
 }  // namespace mtrn3100
